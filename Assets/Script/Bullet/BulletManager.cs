3f8b6bdbac85f7154c6a0785682707fb714722f0
Songmin00 <dn7272@gmail.com>
2025-10-27 11:26:48 +0900

    [add] BulletManager class
    불릿 매니저의 MakeBullet 매서드 추가.

--- Assets/Script/Bullet/BulletManager.cs
+++ Assets/Script/Bullet/BulletManager.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BulletManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<Bullet> _bulletPool;
+    [SerializeField] GameObject _arrow;
+    [SerializeField] GameObject _ball;
+    [SerializeField] GameObject _stone;
+    [SerializeField] GameObject _missile;
+
+    private void Awake()//불릿풀 뉴 할당.
     {
-        
+        _bulletPool = new List<Bullet>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MakeBullet(BulletType type)//불릿 만드는 매서드.
     {
-        
-    }
+        bool isAlive = false;
+
+        for (int i = 0; i < _bulletPool.Count; i++) //타입 맞는 불릿 존재하면 활성화.
+        {
+            if (_bulletPool[i]._type == type && _bulletPool[i].gameObject.activeSelf == false)
+            {
+                _bulletPool[i].gameObject.SetActive(true);
+                isAlive = true;
+                break;
+            }
+        }
+        if (isAlive == false) //없다면 새로 생성해서 등록.
+        {
+            switch (type)
+            {
+                case BulletType.Arrow:
+                    _bulletPool.Add(Instantiate(_arrow).GetComponent<Bullet>());
+                    break;
+                case BulletType.Ball:                    
+                    _bulletPool.Add(Instantiate(_ball).GetComponent<Bullet>());
+                    break;
+                case BulletType.Stone:                    
+                    _bulletPool.Add(Instantiate(_stone).GetComponent<Bullet>());
+                    break;
+                case BulletType.Missile:                    
+                    _bulletPool.Add(Instantiate(_missile).GetComponent<Bullet>());
+                    break;
+            }
+        }
+    }    
 }
