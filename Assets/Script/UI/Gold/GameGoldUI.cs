9f452d3933835e6d76edc46dc3fb86d259b8ab62
GodKong2431 <kizna10231@gmail.com>
2025-10-27 21:55:24 +0900

    [feat] GameGoldUI에 발행자를 구독하고 해제하는 기능 추가
    GameGoldUI에 발행자를 구독하고 해제하는 기능 추가

--- Assets/Script/UI/Gold/GameGoldUI.cs
+++ Assets/Script/UI/Gold/GameGoldUI.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class GameGoldUI : MonoBehaviour
+public class GameGoldUI : MonoBehaviour, IGameGoldObserver
 {
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshProUGUI _GoldText;
+
+    public void OnGameGoldChanged(int Gold)
     {
-        
+        _GoldText.text = 100.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Awake()
     {
-        
+        GameManager.Instance.GameGoldObserver.AddObserver(this);
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.GameGoldObserver.RemoveObserver(this);
     }
 }
