d325af176d38f5abefbd14f6595e50c3eb501b45
kim2134 <ping93852@gmail.com>
2025-10-28 11:35:23 +0900

    [fix, add]테스트EnemyTracker.cs 추가 loop에 EnemyTracker 연동
    테스트EnemyTracker.cs 추가 loop에 EnemyTracker 연동

--- Assets/Script/KHJ/kim/GameLoop.cs
+++ Assets/Script/KHJ/kim/GameLoop.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class GameLoop : MonoBehaviour
 {
-    private bool loop = false;
-    // Start is called before the first frame update
+    private EnemyStatsSO _enemyStats;
+    private bool _isloop = false;
     private void Start()
     {
 
         //스폰 포인트에서 스폰
     }
-    //public void StartLoop()
-    //{
-    //    if (!loop)
-    //    {
-    //        StartCoroutine(Loop());
-    //    }
-    //}
-    //private IEnumerator Loop()
-    //{
-    //    loop = true;
-    //    while (GameManager.Instance.life > 0)
-    //    {
+    public void StartLoop()
+    {
+        if (!_isloop)
+        {
+            StartCoroutine(Loop());
+        }
+        //버튼 누르면 시작
+    }
+    private IEnumerator Loop()
+    {
+        GameManager.Instance.Wave();
+        _isloop = true;
+        EnemySpawner.Instance.Spawn(_enemyStats, count: 5, pathId: 0, interval: 0.8f);
 
-    //    }
-    //}
+        yield return null; 
+    }
     private void StartWave()
     {
 
