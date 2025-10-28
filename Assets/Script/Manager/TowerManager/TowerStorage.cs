90e49b5e47b97ac37931d6c0b1b1571b40157410
Songmin00 <dn7272@gmail.com>
2025-10-28 17:01:42 +0900

    [fix] TowerStorage
    TowerStorage의 딕셔너리 필드에 자동 프로퍼티 추가.

--- Assets/Script/Manager/TowerManager/TowerStorage.cs
+++ Assets/Script/Manager/TowerManager/TowerStorage.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class TowerStorage : SingleTon<TowerStorage>
 {
-    public Dictionary<TowerType, GameObject> _basicTowers;
-    public Dictionary<TowerType, GameObject> _advancedTowers;
-    public Dictionary<TowerType, GameObject> _finalTowers;
+    public Dictionary<TowerType, GameObject> BasicTowers {  get; private set; }
+    public Dictionary<TowerType, GameObject> AdvancedTowers { get; private set; }
+    public Dictionary<TowerType, GameObject> FinalTowers { get; private set; }
 
     [SerializeField] GameObject _ballista_0;
     [SerializeField] GameObject _ballista_1;
@@ -35,30 +35,30 @@ public class TowerStorage : SingleTon<TowerStorage>
 
     private void SetStorage()
     {
-        _basicTowers = new Dictionary<TowerType, GameObject>();
-        _advancedTowers = new Dictionary<TowerType, GameObject>();
-        _finalTowers = new Dictionary<TowerType, GameObject>();
+        BasicTowers = new Dictionary<TowerType, GameObject>();
+        AdvancedTowers = new Dictionary<TowerType, GameObject>();
+        FinalTowers = new Dictionary<TowerType, GameObject>();
 
-        _basicTowers.Add(TowerType.Ballista, _ballista_0);
-        _basicTowers.Add(TowerType.Catapult, _catapult_0);
-        _basicTowers.Add(TowerType.Cannon, _cannon_0);
-        _basicTowers.Add(TowerType.Turret, _turret_0);
-        _basicTowers.Add(TowerType.Buff, _buffTower_0);
-        _basicTowers.Add(TowerType.Debuff, _debuffTower_0);
+        BasicTowers.Add(TowerType.Ballista, _ballista_0);
+        BasicTowers.Add(TowerType.Catapult, _catapult_0);
+        BasicTowers.Add(TowerType.Cannon, _cannon_0);
+        BasicTowers.Add(TowerType.Turret, _turret_0);
+        BasicTowers.Add(TowerType.Buff, _buffTower_0);
+        BasicTowers.Add(TowerType.Debuff, _debuffTower_0);
 
-        _advancedTowers.Add(TowerType.Ballista, _ballista_1);
-        _advancedTowers.Add(TowerType.Catapult, _catapult_1);
-        _advancedTowers.Add(TowerType.Cannon, _cannon_1);
-        _advancedTowers.Add(TowerType.Turret, _turret_1);
-        _advancedTowers.Add(TowerType.Buff, _buffTower_1);
-        _advancedTowers.Add(TowerType.Debuff, _debuffTower_1);
+        AdvancedTowers.Add(TowerType.Ballista, _ballista_1);
+        AdvancedTowers.Add(TowerType.Catapult, _catapult_1);
+        AdvancedTowers.Add(TowerType.Cannon, _cannon_1);
+        AdvancedTowers.Add(TowerType.Turret, _turret_1);
+        AdvancedTowers.Add(TowerType.Buff, _buffTower_1);
+        AdvancedTowers.Add(TowerType.Debuff, _debuffTower_1);
 
-        _finalTowers.Add(TowerType.Ballista, _ballista_2);
-        _finalTowers.Add(TowerType.Catapult, _catapult_2);
-        _finalTowers.Add(TowerType.Cannon, _cannon_2);
-        _finalTowers.Add(TowerType.Turret, _turret_2);
-        _finalTowers.Add(TowerType.Buff, _buffTower_2);
-        _finalTowers.Add(TowerType.Debuff, _debuffTower_2);
+        FinalTowers.Add(TowerType.Ballista, _ballista_2);
+        FinalTowers.Add(TowerType.Catapult, _catapult_2);
+        FinalTowers.Add(TowerType.Cannon, _cannon_2);
+        FinalTowers.Add(TowerType.Turret, _turret_2);
+        FinalTowers.Add(TowerType.Buff, _buffTower_2);
+        FinalTowers.Add(TowerType.Debuff, _debuffTower_2);
     }
 
 }
--- Assets/Script/Manager/TowerManager/UpgradeManager.cs
+++ Assets/Script/Manager/TowerManager/UpgradeManager.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 
 public class UpgradeManager : SingleTon<UpgradeManager>
 {
-    public GameObject BuildTower(TowerType type)
+    public GameObject BuildTower(TowerType type) // 맨땅에 새로운 타워 생성 매서드.
     {
-        GameObject newTower = Instantiate(TowerStorage.Instance._basicTowers[type]);
+        GameObject newTower = Instantiate(TowerStorage.Instance.BasicTowers[type]);
         newTower.transform.position = Vector3.zero;//레이캐스트 위치로 생성
         return newTower;
     }
 
-    //public GameObject UpgradeTower(GameObject tower)
-    //{
-    //    switch (tower.GetComponent<TowerBase>().)//원래는 레벨 받아야 함
-    //    {
-    //        case 0:
-    //            break;
-    //    }
-
-    //}
-
-
-    
+    public GameObject UpgradeTower(GameObject tower)//기존 타워 업그레이드 매서드.
+    {
+        GameObject newTower = null;
+        TowerBase towerBase = tower.GetComponent<TowerBase>();
+        TowerType type = towerBase.type;
+        int level = towerBase.level; 
+        switch (level)
+        {
+            case 0:
+                newTower = Instantiate(TowerStorage.Instance.AdvancedTowers[type]);
+                Destroy(tower);
+                return newTower;
+            case 1:
+                newTower = Instantiate(TowerStorage.Instance.FinalTowers[type]);
+                Destroy(tower);
+                return newTower;
+            case 2:
+                Debug.Log("이미 최고 레벨인 타워입니다!");
+                return null;
+        }
+    }
 }
