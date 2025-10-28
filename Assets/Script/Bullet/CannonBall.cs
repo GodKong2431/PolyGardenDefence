5a26ea226a185d1daabdcc84b5a9546647ec2409
Songmin00 <dn7272@gmail.com>
2025-10-28 14:18:14 +0900

    [fix] CannonBall
    타워가 적과 너무 가까우면 CannonBall이 제자리에서 폭발하는 문제 수정.
    TowerManager 폴더 생성.
    BulletManager 스크립트 파일 위치 변경.

--- Assets/Script/Bullet/CannonBall.cs
+++ Assets/Script/Bullet/CannonBall.cs
@@ -6,34 +6,42 @@ using UnityEngine;
 public class CannonBall : BulletBase
 {
     [SerializeField] float _explosionRadius;
+    float _detectRange = 0;
     
+        
     protected override void OnTriggerEnter(Collider other)
     {
-        Explode();        
+        if (other.gameObject.CompareTag("Tower") || other.gameObject.CompareTag("Bullet"))
+        {
+            return;
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            Explode();            
+        }
+        OffBullet();
     }
 
-    
     private void Explode()
     {
         Collider[] _hitEnemies;
-        _hitEnemies = Physics.OverlapSphere(transform.position, _explosionRadius);        
+        _hitEnemies = Physics.OverlapSphere(transform.position, _explosionRadius);
         foreach (var hit in _hitEnemies)
         {
-            //대포 및 대포알 스스로는 감지하지 않기.
+            //대포 및 대포알이나 다른 총알은 감지하지 않기.
             if (hit.gameObject.CompareTag("Bullet") || hit.gameObject.CompareTag("Tower"))
             {
-                continue; 
+                continue;
             }
-
             if (hit != null)
             {
                 GiveDamage(hit);
-                Debug.Log("맞은놈 : " + hit.name);
-                OffBullet();
+                Debug.Log("맞은놈 : " + hit.name);                
             }
+            OffBullet();
         }
-        
     }
+
 
     //폭발반경 확인
     private void OnDrawGizmosSelected()
