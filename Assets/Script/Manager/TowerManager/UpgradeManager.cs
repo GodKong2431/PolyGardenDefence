139b09c6851858c49a74d90b3439a107b4282a6a
Songmin00 <dn7272@gmail.com>
2025-10-29 04:20:47 +0900

    [add] UpgradeManager
    타워 제작 및 배치를 담당하던 BuildTower 매서드를 제작 담당 BuildTower 매서드와 배치 담당 PlaceTower 매서드로 분화.
    타워 판매용 SellTower 매서드 추가.
    TowerStorage 헤더 추가.

--- Assets/Script/Manager/TowerManager/UpgradeManager.cs
+++ Assets/Script/Manager/TowerManager/UpgradeManager.cs
@@ -5,32 +5,54 @@ using UnityEngine;
 
 public class UpgradeManager : SingleTon<UpgradeManager>
 {
-    public GameObject BuildTower(TowerType type) // 맨땅에 새로운 타워 생성 매서드.
+    
+    public GameObject BuildTower(TowerType type) // 타입에 맞는 타워 생성 후 비활성화
     {
-        GameObject newTower = Instantiate(TowerStorage.Instance.BasicTowers[type]);
-        newTower.transform.position = Vector3.zero;//레이캐스트 위치로 생성
+        GameObject newTower = null;
+        newTower = Instantiate(TowerStorage.Instance.BasicTowers[type]);
+        newTower.SetActive(false);        
         return newTower;
+    }
+
+    public void PlaceTower(GameObject tower, Vector3 position) //위치값 받아서 타워 배치 후 활성화.
+    {
+        GameObject newTower = tower;
+        newTower.transform.position = position;
+        newTower.SetActive(true);
     }
 
     //public GameObject UpgradeTower(GameObject tower)//기존 타워 업그레이드 매서드.
     //{
-    //    GameObject newTower = null;
+    //    GameObject upgradedTower = null;
     //    TowerBase towerBase = tower.GetComponent<TowerBase>();
     //    TowerType type = towerBase.type;
-    //    int level = towerBase.level; 
+    //    int level = towerBase.level;
     //    switch (level)
     //    {
     //        case 0:
-    //            newTower = Instantiate(TowerStorage.Instance.AdvancedTowers[type]);
+    //            upgradedTower = Instantiate(TowerStorage.Instance.AdvancedTowers[type], tower.transform.position, tower.transform.rotation);
     //            Destroy(tower);
-    //            return newTower;
+    //            return upgradedTower;
     //        case 1:
-    //            newTower = Instantiate(TowerStorage.Instance.FinalTowers[type]);
+    //            upgradedTower = Instantiate(TowerStorage.Instance.FinalTowers[type], tower.transform.position, tower.transform.rotation);
     //            Destroy(tower);
-    //            return newTower;
+    //            return upgradedTower;
     //        case 2:
     //            Debug.Log("이미 최고 레벨인 타워입니다!");
     //            return null;
+    //        default:
+    //            Debug.Log("잘못된 타워 레벨 설정입니다.");
+    //            return null;
     //    }
     //}
+
+    public void SellTower(GameObject tower)
+    {
+        TowerBase towerBase = tower.GetComponent<TowerBase>();
+        if (towerBase != null)
+        {
+            Destroy(tower);
+            //GameManager.Instance.SubGold(towerBase._price);
+        }
+    }
 }
