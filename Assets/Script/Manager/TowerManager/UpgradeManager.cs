fc64d67dcc6758021922828f57fc80df5330367d
Songmin00 <dn7272@gmail.com>
2025-10-31 11:56:30 +0900

    [fix] TowerPrefabs, UpgradeManager
    타워 프리팹 콜라이더 수정.
    배치 및 업그레이드 비용 지불하도록 수정.

--- Assets/Script/Manager/TowerManager/UpgradeManager.cs
+++ Assets/Script/Manager/TowerManager/UpgradeManager.cs
@@ -16,13 +16,21 @@ public class UpgradeManager : SingleTon<UpgradeManager>
 
     public void PlaceTower(TowerType type, GameObject tile) //타일 받아서 타워 배치.
     {
-        TowerSpot spot = tile.GetComponent<TowerSpot>();
+        TowerSpot spot = tile.GetComponentInChildren<TowerSpot>();
         if (spot.PlacedTower == null)
         {
             GameObject newTower = BuildTower(type);
             newTower.transform.position = spot.GetPlacePosition();
-            GameManager.Instance.SubGold(newTower.GetComponent<TowerBase>().Price);
-            spot.Occupy(newTower);
+            if (GameManager.Instance.SubGold(newTower.GetComponentInChildren<TowerBase>().Price) == true)
+            {
+                spot.Occupy(newTower);
+            }
+            else
+            {
+                Debug.Log("타워를 설치할 비용이 부족합니다.");
+                Destroy(newTower);
+            }
+                
         }
         else
         {
@@ -31,18 +39,20 @@ public class UpgradeManager : SingleTon<UpgradeManager>
         }        
     }
 
-    public GameObject UpgradeTower(TowerBase towerBase)//기존 타워 업그레이드 매서드.
+    public GameObject UpgradeTower(TowerBase selectedTower)//기존 타워 업그레이드 매서드.
     {
+        int cost;
         GameObject upgradedTower = null;
-        TowerType type = towerBase.TowerType;
-        int level = towerBase.Level;
+        TowerType type = selectedTower.TowerType;
+        int level = selectedTower.Level;
+
         switch (level)
         {
             case 1:
-                upgradedTower = Instantiate(TowerStorage.Instance.AdvancedTowers[type], towerBase.transform.position, towerBase.transform.rotation);
+                upgradedTower = Instantiate(TowerStorage.Instance.AdvancedTowers[type], selectedTower.transform.position, selectedTower.transform.rotation);
                 break;
             case 2:
-                upgradedTower = Instantiate(TowerStorage.Instance.FinalTowers[type], towerBase.transform.position, towerBase.transform.rotation);
+                upgradedTower = Instantiate(TowerStorage.Instance.FinalTowers[type], selectedTower.transform.position, selectedTower.transform.rotation);
                 break;
             case 3:
                 Debug.Log("이미 최고 레벨인 타워입니다!");
@@ -51,9 +61,19 @@ public class UpgradeManager : SingleTon<UpgradeManager>
                 Debug.Log("잘못된 타워 레벨 설정입니다.");
                 return null;
         }
-        Destroy(towerBase.gameObject);        
-        towerBase.Tile.GetComponent<TowerSpot>().Occupy(upgradedTower);
-        return upgradedTower;
+        cost = upgradedTower.GetComponentInChildren<TowerBase>().Price - selectedTower.Price;
+
+        if (GameManager.Instance.SubGold(cost) == true)
+        {
+            Destroy(selectedTower.gameObject);
+            selectedTower.Tile.GetComponentInChildren<TowerSpot>().Occupy(upgradedTower);
+            return upgradedTower;
+        }
+        else
+        {
+            Debug.Log("업그레이드 비용이 부족합니다.");
+            return null;
+        }
     }
 
     public void SellTower(TowerBase towerBase)
---
+++ Assets/Script/Tower/TurretTower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretTower : TowerBase
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // 인스펙터 연결이 비어있으면 한 번 자동 검색
+        if (_bulletManager == null)
+        {
+            _bulletManager = FindFirstObjectByType<BulletManager>();
+            if (_bulletManager == null)
+            {
+                Debug.LogError("[TurretTower] BulletManager reference is missing. Please assign in Inspector.");
+            }
+        }
+    }
+}
